cbb4dd12e1939509999ae27e3d3d1ad84c0a7b19
vcadalso <vanessa.cadalso@gmail.com>
2024-03-25 13:46:18 -0400

    Added css styling

--- src/Weather.js
+++ src/Weather.js
@@ -7,9 +7,9 @@ export default function Weather () {
         <form>
             <div className="row">
             <div className="col-9">
-            <input type="search"  placeholder= "Enter a city..." className="form-control"/> </div>
+            <input type="search"  placeholder= "Enter a city..." className="form-control" autoFocus="on"/> </div>
             <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary"/> </div> </div>
+            <input type="submit" value="Search" className="btn btn-primary w-100"/> </div> </div>
         </form>
         <h1>New York</h1>
         <ul>
@@ -20,9 +20,15 @@ export default function Weather () {
                 Mostly Cloudy
             </li>
             </ul>
-            <div className="row">
+            <div className="row mt-3">
                 <div className="col-6">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly Cloudy"/> 44°F
+                    <div className="clearfix">
+                    <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly Cloudy" className="float-left mb-4 "/> 
+                    
+                    <span className="temperature">44</span>
+                    <span className="unit">°F</span>
+               
+                </div>
                 </div>
                 <div className="col-6">
                 <ul>
@@ -36,6 +42,7 @@ export default function Weather () {
                         Wind:
                     </li>
                 </ul>
+                
                 </div>
              </div>
         </div>
