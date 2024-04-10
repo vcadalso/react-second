2b47996bb84c21a1b58530cb79e21d679316ee10
vcadalso <vanessa.cadalso@gmail.com>
2024-04-10 13:41:58 -0400

    Added beginning of forecast structure

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import WeatherForecast from "./WeatherForecast";
-
 import axios from "axios";
 import "./Weather.css";
 import WeatherInfo from "./WeatherInfo";
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -19,7 +19,7 @@ export default function WeatherInfo (props) {
             <div className="row mt-3">
                 <div className="col-6">
                     <div className="d-flex">
-                        <div className="float-left"><WeatherIcon code={props.data.icon} /></div>
+                        <div className="float-left"><WeatherIcon code={props.data.icon} size={52} /></div>
                     
                     
                    <WeatherTemperature celsius= {props.data.temperature} />
