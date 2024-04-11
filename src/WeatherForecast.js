51593ab2b24119419dee94b1c15c4b287c5a9ea5
vcadalso <vanessa.cadalso@gmail.com>
2024-04-11 09:33:38 -0400

    fixed apiUrl

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -13,7 +13,7 @@ export default function WeatherForecast(props) {
     let apiKey = "502dc8f7ae36e57af1974e18d16a86f8";
     let longitude = props.coordinates.lon; 
     let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
 
 
     axios.get(apiUrl).then(handleResponse);
