a8d14bdcd7a39f8c43c8c57b913bda44d48f0feb
vcadalso <vanessa.cadalso@gmail.com>
2024-04-12 14:00:53 -0400

    Finishing touches

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -7,6 +7,10 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
     let [loaded, setLoaded] = useState(false);
     let [forecast, setForecast] = useState(null);
+
+    useEffect(() => {
+        setLoaded(false);
+    }, [props.coordinates]);
     
     function handleResponse(response) {
         setForecast(response.data.daily);
@@ -24,6 +28,8 @@ export default function WeatherForecast(props) {
             <WeatherForecastDay data={dailyForecast} />
         </div>
             );
+        } else {
+            return null;
         }
 
 } ) }   
