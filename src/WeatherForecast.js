218693e75320dd7fc5060f6a21b605c99d510d18
vcadalso <vanessa.cadalso@gmail.com>
2024-04-12 13:39:04 -0400

    Updated forecast with real data

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,37 +1,47 @@
 
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
+    let [loaded, setLoaded] = useState(false);
+    let [forecast, setForecast] = useState(null);
+    
     function handleResponse(response) {
-   console.log(response.data);
+        setForecast(response.data.daily);
+        setLoaded (true);
     }
 
-
-    let apiKey = "502dc8f7ae36e57af1974e18d16a86f8";
-    let longitude = props.coordinates.lon; 
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
-
-
-    axios.get(apiUrl).then(handleResponse);
-    
+    if (loaded) {
     return ( 
     <div className="WeatherForecast">
     <div className="row">
-         <div className="col">
-            <div className="WeatherForecast-day">Thu</div> 
-            <WeatherIcon code="01d" size={36}/>
-            <div className="WeatherForecast-temp"> 
-            <span className="WeatherForecast-temp-max">19°</span> 
-            <span className="WeatherForecast-temp-min">10°</span>
-            </div> 
+         {forecast.map(function (dailyForecast, index) {
+        if (index < 5) {
+            return (
+                <div className="col" key={index}>
+            <WeatherForecastDay data={dailyForecast} />
         </div>
-    </div>
+            );
+        }
+
+} ) }   
+</div>
  </div>
     );
+    } else {
+        let apiKey = "502dc8f7ae36e57af1974e18d16a86f8";
+    let longitude = props.coordinates.lon; 
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+ 
+    }
+
 }
 
 
